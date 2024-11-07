1bf147e38be8e7bf39a6dbddb95adc7d7d68ebde
Ernesto Fernandez <fernandez.ernes@gmail.com>
2024-11-07 14:17:58 -0300

    Finalizado el CRUD de todos los servicios

--- microservice-stop/src/main/java/org/arquitecturas/grupo17/microservicestop/controller/StopController.java
+++ microservice-stop/src/main/java/org/arquitecturas/grupo17/microservicestop/controller/StopController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("api/stop")
 public class StopController {
     private final StopService stopService;
 
